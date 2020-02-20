1d0b9d31518240b08db8247ccdbd4d229421b852
luffare97 <daniel.agren@hotmail.se>
2020-02-20 15:17:51 +0100

    Ny aktivitet

--- Laboration2/GUI/LoggIn.Designer.cs
+++ Laboration2/GUI/LoggIn.Designer.cs
@@ -28,13 +28,130 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.LoggaInBtn = new System.Windows.Forms.Button();
+            this.IDTxt = new System.Windows.Forms.TextBox();
+            this.LösenordTxt = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.RegistreraBtn = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.LösenordTxt);
+            this.groupBox1.Controls.Add(this.IDTxt);
+            this.groupBox1.Controls.Add(this.LoggaInBtn);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(292, 108);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Logga in";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Användar ID:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Lösenord:";
+            // 
+            // LoggaInBtn
+            // 
+            this.LoggaInBtn.Location = new System.Drawing.Point(200, 71);
+            this.LoggaInBtn.Name = "LoggaInBtn";
+            this.LoggaInBtn.Size = new System.Drawing.Size(75, 23);
+            this.LoggaInBtn.TabIndex = 2;
+            this.LoggaInBtn.Text = "Logga in";
+            this.LoggaInBtn.UseVisualStyleBackColor = true;
+            // 
+            // IDTxt
+            // 
+            this.IDTxt.Location = new System.Drawing.Point(82, 19);
+            this.IDTxt.Name = "IDTxt";
+            this.IDTxt.Size = new System.Drawing.Size(193, 20);
+            this.IDTxt.TabIndex = 3;
+            // 
+            // LösenordTxt
+            // 
+            this.LösenordTxt.Location = new System.Drawing.Point(82, 45);
+            this.LösenordTxt.Name = "LösenordTxt";
+            this.LösenordTxt.Size = new System.Drawing.Size(193, 20);
+            this.LösenordTxt.TabIndex = 4;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.RegistreraBtn);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Location = new System.Drawing.Point(12, 126);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(292, 43);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 16);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(111, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Eller registrera dig här:";
+            // 
+            // RegistreraBtn
+            // 
+            this.RegistreraBtn.Location = new System.Drawing.Point(200, 11);
+            this.RegistreraBtn.Name = "RegistreraBtn";
+            this.RegistreraBtn.Size = new System.Drawing.Size(75, 23);
+            this.RegistreraBtn.TabIndex = 1;
+            this.RegistreraBtn.Text = "Registrera dig";
+            this.RegistreraBtn.UseVisualStyleBackColor = true;
+            // 
+            // LoggIn
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(324, 184);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "LoggIn";
+            this.Text = "Logga in";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox LösenordTxt;
+        private System.Windows.Forms.TextBox IDTxt;
+        private System.Windows.Forms.Button LoggaInBtn;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button RegistreraBtn;
+        private System.Windows.Forms.Label label3;
     }
 }
 
---
+++ Laboration2/GUI/NyAktivitet.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace GUI
+{
+    partial class NyAktivitet
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.TillbakaBtn = new System.Windows.Forms.Button();
+            this.SparaBtn = new System.Windows.Forms.Button();
+            this.TitelTxt = new System.Windows.Forms.TextBox();
+            this.PlatserField = new System.Windows.Forms.NumericUpDown();
+            this.StartDateTime = new System.Windows.Forms.DateTimePicker();
+            this.SlutDateTime = new System.Windows.Forms.DateTimePicker();
+            this.PlatsTxt = new System.Windows.Forms.TextBox();
+            this.BeskrivningTxt = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.PlatserField)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Titel på aktiviteten:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Antal platser:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 70);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Start tid:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 96);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Slut tid:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 119);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(33, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Plats:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 145);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(65, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Beskrivning:";
+            // 
+            // TillbakaBtn
+            // 
+            this.TillbakaBtn.Location = new System.Drawing.Point(15, 297);
+            this.TillbakaBtn.Name = "TillbakaBtn";
+            this.TillbakaBtn.Size = new System.Drawing.Size(75, 23);
+            this.TillbakaBtn.TabIndex = 6;
+            this.TillbakaBtn.Text = "Tillbaka";
+            this.TillbakaBtn.UseVisualStyleBackColor = true;
+            // 
+            // SparaBtn
+            // 
+            this.SparaBtn.Location = new System.Drawing.Point(250, 297);
+            this.SparaBtn.Name = "SparaBtn";
+            this.SparaBtn.Size = new System.Drawing.Size(75, 23);
+            this.SparaBtn.TabIndex = 7;
+            this.SparaBtn.Text = "Spara";
+            this.SparaBtn.UseVisualStyleBackColor = true;
+            // 
+            // TitelTxt
+            // 
+            this.TitelTxt.Location = new System.Drawing.Point(125, 12);
+            this.TitelTxt.Name = "TitelTxt";
+            this.TitelTxt.Size = new System.Drawing.Size(200, 20);
+            this.TitelTxt.TabIndex = 8;
+            // 
+            // PlatserField
+            // 
+            this.PlatserField.Location = new System.Drawing.Point(125, 38);
+            this.PlatserField.Name = "PlatserField";
+            this.PlatserField.Size = new System.Drawing.Size(200, 20);
+            this.PlatserField.TabIndex = 9;
+            // 
+            // StartDateTime
+            // 
+            this.StartDateTime.Location = new System.Drawing.Point(125, 64);
+            this.StartDateTime.Name = "StartDateTime";
+            this.StartDateTime.Size = new System.Drawing.Size(200, 20);
+            this.StartDateTime.TabIndex = 10;
+            // 
+            // SlutDateTime
+            // 
+            this.SlutDateTime.Location = new System.Drawing.Point(125, 90);
+            this.SlutDateTime.Name = "SlutDateTime";
+            this.SlutDateTime.Size = new System.Drawing.Size(200, 20);
+            this.SlutDateTime.TabIndex = 11;
+            // 
+            // PlatsTxt
+            // 
+            this.PlatsTxt.Location = new System.Drawing.Point(125, 116);
+            this.PlatsTxt.Name = "PlatsTxt";
+            this.PlatsTxt.Size = new System.Drawing.Size(200, 20);
+            this.PlatsTxt.TabIndex = 12;
+            // 
+            // BeskrivningTxt
+            // 
+            this.BeskrivningTxt.Location = new System.Drawing.Point(125, 142);
+            this.BeskrivningTxt.Name = "BeskrivningTxt";
+            this.BeskrivningTxt.Size = new System.Drawing.Size(200, 149);
+            this.BeskrivningTxt.TabIndex = 13;
+            this.BeskrivningTxt.Text = "";
+            // 
+            // NyAktivitet
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(342, 336);
+            this.Controls.Add(this.BeskrivningTxt);
+            this.Controls.Add(this.PlatsTxt);
+            this.Controls.Add(this.SlutDateTime);
+            this.Controls.Add(this.StartDateTime);
+            this.Controls.Add(this.PlatserField);
+            this.Controls.Add(this.TitelTxt);
+            this.Controls.Add(this.SparaBtn);
+            this.Controls.Add(this.TillbakaBtn);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "NyAktivitet";
+            this.Text = "Ny aktivitet";
+            ((System.ComponentModel.ISupportInitialize)(this.PlatserField)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button TillbakaBtn;
+        private System.Windows.Forms.Button SparaBtn;
+        private System.Windows.Forms.TextBox TitelTxt;
+        private System.Windows.Forms.NumericUpDown PlatserField;
+        private System.Windows.Forms.DateTimePicker StartDateTime;
+        private System.Windows.Forms.DateTimePicker SlutDateTime;
+        private System.Windows.Forms.TextBox PlatsTxt;
+        private System.Windows.Forms.RichTextBox BeskrivningTxt;
+    }
+}
