0024e4b528ba51f7ab85d589f727807a0f017d17
luffare97 <daniel.agren@hotmail.se>
2020-03-11 14:50:15 +0100

    observer pattern

---
+++ Laboration2/BusinessEntites/Observatorypattern/Subject.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessEntites.Observatorypattern
+{
+    public class Subject
+    {
+
+        public List<Observer> ObserverCollection { get; set; }
+
+        public void RegisterObserver(Observer O)
+        {
+            ObserverCollection.Add(O);
+        }
+
+        public void UnregisterObserver(Observer O)
+        {
+            ObserverCollection.Remove(O);
+        }
+
+        public void Notify()
+        {
+            foreach (Observer O in ObserverCollection)
+            {
+                O.Uppdate();
+            }
+        }
+    }
+}
